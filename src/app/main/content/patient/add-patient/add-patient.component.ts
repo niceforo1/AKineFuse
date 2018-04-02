d578999874da6e62dc24fc5b0b0e28965a1e0571
franc363 <pela363@gmail.com>
2018-04-02 16:16:36 -0300

    Se agregan los autocomplete en provincia, ciudad y obra social para agregar y editar paciente

--- src/app/main/content/patient/add-patient/add-patient.component.ts
+++ src/app/main/content/patient/add-patient/add-patient.component.ts
@@ -1,19 +1,26 @@
-import {Component, OnInit} from '@angular/core';
-import {Router, ActivatedRoute} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 /* Services */
-import {PatientService} from '../../../services/patient.service';
-import {SocialInsuranceService} from '../../../services/socialInsurance.service';
-import {LocationService} from '../../../services/location.service';
+import { PatientService } from '../../../services/patient.service';
+import { SocialInsuranceService } from '../../../services/socialInsurance.service';
+import { LocationService } from '../../../services/location.service';
 /* Models */
-import {Patient} from '../../../models/patient';
-import {PatientSocialInsurance} from '../../../models/patientSocialInsurance';
-import {Address} from '../../../models/Address';
-import {Phone} from '../../../models/Phone';
-import {SocialInsurance} from '../../../models/SocialInsurance';
+import { Patient } from '../../../models/patient';
+import { PatientSocialInsurance } from '../../../models/patientSocialInsurance';
+import { Address } from '../../../models/Address';
+import { Phone } from '../../../models/Phone';
+import { SocialInsurance } from '../../../models/SocialInsurance';
 /*Dialog*/
-import { DialogConfigComponent }from '../../dialog/dialogConfig.component';
+import { DialogConfigComponent } from '../../dialog/dialogConfig.component';
 /*Alert*/
 import { AlertComponent } from '../../alerts/alert.component';
+/*Autocomplete*/
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
+import { startWith } from 'rxjs/operators/startWith';
+import { map } from 'rxjs/operators/map';
 
 @Component({
   selector: 'app-add-patient',
@@ -26,27 +33,37 @@ import { AlertComponent } from '../../alerts/alert.component';
     AlertComponent
   ]
 })
-
 export class AddPatientComponent implements OnInit {
   action: string;
   title: string;
-  message: string;
-  messageClass: string;
   patient: any;
   socialInsurances: any;
-  socialInsurance: any;
-  id_patient = "new";
-  province: any;
-  provinces:any;
-  cities:any;
+  socialInsurance: SocialInsurance;
+  id_patient = 'new';
+  provinces: any;
+  cities: any;
 
-  constructor(private _patientService: PatientService, private _socialInsuranceService: SocialInsuranceService,
-              private _locationService:LocationService, private _router: Router, private _activatedRoute: ActivatedRoute,
-              private dialog : DialogConfigComponent, public alert: AlertComponent) {
+  myControlProv = new FormControl();
+  myControlCity = new FormControl();
+  myControlSIns = new FormControl();
+  filteredOptionsProv: Observable<any[]>;
+  filteredOptionsSIns: Observable<any[]>;
+  filteredOptionsCity: Observable<any[]>;
+
+  constructor(
+    private _patientService: PatientService,
+    private _socialInsuranceService: SocialInsuranceService,
+    private _locationService: LocationService,
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute,
+    private dialog: DialogConfigComponent,
+    public alert: AlertComponent
+  ) {
     this.action = 'Guardar';
     this.title = 'Agregar Paciente';
     this.patient = new Patient();
     this.patient.socialInsurance = new PatientSocialInsurance();
+    this.socialInsurance = new SocialInsurance();
     /*PHONE*/
     this.patient.phones = new Phone();
     this.patient.phones.main = true;
@@ -55,7 +72,8 @@ export class AddPatientComponent implements OnInit {
     this.patient.address = new Address();
     /*SOCIAL INSURANCE*/
     this.socialInsurances = new Array();
-    this.socialInsurance = new PatientSocialInsurance();
+    /*Provincias*/
+    this.provinces = new Array();
   }
 
   ngOnInit() {
@@ -67,46 +85,122 @@ export class AddPatientComponent implements OnInit {
     this.savePatient();
   }
 
-  onChange(){
-    this._locationService.getCities(this.province).subscribe(data => {
-      this.patient.address.state = this.province;
-      this.cities = data[0].localidad;
-    },
-    err => {
-      this.alert.openErrorSnackBar(this.alert.genericError);
-    })
+  onChange() {
+    if (!this.patient.address.state) {
+      return;
+    }
+    this.patient.address.city = null;
+    this._locationService.getCities(this.patient.address.state).subscribe(
+      data => {
+        if (data[0] === undefined) {
+          this.cities = [];
+          this.filteredOptionsCity = null;
+          return;
+        }
+        this.cities = data[0].localidad;
+        this.filteredOptionsCity = this.myControlCity.valueChanges
+          .startWith(null)
+          .map(city => (city ? this.filterCity(city) : this.cities.slice()));
+        return;
+      },
+      err => {
+        this.alert.openErrorSnackBar(this.alert.genericError);
+      }
+    );
   }
 
   savePatient() {
-    this._patientService.getPatientByDoc(this.patient.id).subscribe(data => {
-      if (data) {
-        this.alert.openErrorSnackBar(this.alert.errorDuplicado);
-      } else {
-        this._patientService.savePatient(this.patient).subscribe(data => {
-          this.alert.openSuccessSnackBar(this.alert.successPatient);
-        });
+    this.patient.socialInsurance._id = this.socialInsurance._id;
+    this._patientService.getPatientByDoc(this.patient.id).subscribe(
+      data => {
+        if (data) {
+          this.alert.openErrorSnackBar(this.alert.errorDuplicado);
+        } else {
+          this._patientService.savePatient(this.patient).subscribe(data => {
+            this.alert.openSuccessSnackBar(this.alert.successPatient);
+          });
+        }
+      },
+      err => {
+        this.alert.openErrorSnackBar(this.alert.genericError);
       }
-    },
-    err => {
-      this.alert.openErrorSnackBar(this.alert.genericError);
-    });
+    );
   }
 
   getSocialInsurances() {
-    this._socialInsuranceService.getSocialInsurance().subscribe(response => {
-      this.socialInsurances = response;
-    },
-    err => {
-      this.alert.openErrorSnackBar(this.alert.genericError);
+    this._socialInsuranceService.getSocialInsurance().subscribe(
+      response => {
+        this.socialInsurances = response;
+        this.filteredOptionsSIns = this.myControlSIns.valueChanges
+          .startWith(null)
+          .map(
+            sIns => (sIns ? this.filterSIns(sIns) : this.socialInsurances.slice())
+          );
+      },
+      err => {
+        this.alert.openErrorSnackBar(this.alert.genericError);
+      }
+    );
+  }
+
+  getProvinces() {
+    this._locationService.getProvinces().subscribe(
+      data => {
+        this.provinces = data;
+        this.filteredOptionsProv = this.myControlProv.valueChanges
+          .startWith(null)
+          .map(
+            prov => (prov ? this.filterProvinces(prov) : this.provinces.slice())
+          );
+      },
+      err => {
+        this.alert.openErrorSnackBar(this.alert.genericError);
+      }
+    );
+  }
+
+  filterProvinces(prov: string): any[] {
+    return this.provinces.filter(option => {
+      return option.provincia.toLowerCase().indexOf(prov.toLowerCase()) === 0;
     });
   }
-  getProvinces(){
-    this._locationService.getProvinces().subscribe(data => {
-      this.provinces = data;
-    },
-    err => {
-      this.alert.openErrorSnackBar(this.alert.genericError);
+
+  filterCity(city: string): any[] {
+    return this.cities.filter(option => {
+      return option.toLowerCase().indexOf(city.toLowerCase()) === 0;
     });
   }
+
+  filterSIns(socIns: any): any[] {
+    if(typeof(socIns) == 'object'){
+      return this.socialInsurances.filter(option => {
+        return option.name.toLowerCase().indexOf(socIns.name.toLowerCase()) === 0;
+      });
+    };
+    if(typeof(socIns) == 'string'){
+      return this.socialInsurances.filter(option => {
+        return option.name.toLowerCase().indexOf(socIns.toLowerCase()) === 0;
+      });
+    }
+  }
+
+  displayFnProv(prov: string): string {
+    if (prov) {
+      return prov;
+    }
+  }
+
+  displayFnCity(city: string): string {
+    if (city) {
+      return city;
+    }
+  }
+
+  displaySIns(socIns: any): string {
+    if (socIns) {
+      return socIns.name;
+    }
+  }
+
 
 }
--- src/app/main/content/patient/edit-patient/edit-patient.component.ts
+++ src/app/main/content/patient/edit-patient/edit-patient.component.ts
@@ -3,15 +3,24 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 /* Services */
 import {PatientService} from '../../../services/patient.service';
 import {SocialInsuranceService} from '../../../services/socialInsurance.service';
+import { LocationService } from '../../../services/location.service';
 /* Models */
 import {Patient} from '../../../models/patient';
 import {PatientSocialInsurance} from '../../../models/patientSocialInsurance';
 import {Address} from '../../../models/Address';
 import {Phone} from '../../../models/Phone';
+import {SocialInsurance} from '../../../models/SocialInsurance';
 /*Dialog*/
 import { DialogConfigComponent }from '../../dialog/dialogConfig.component';
 /*Alert*/
 import { AlertComponent } from '../../alerts/alert.component';
+/*Autocomplete*/
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
+import { startWith } from 'rxjs/operators/startWith';
+import { map } from 'rxjs/operators/map';
 
 @Component({
   selector: 'app-edit-patient',
@@ -19,6 +28,7 @@ import { AlertComponent } from '../../alerts/alert.component';
   providers: [
     PatientService,
     SocialInsuranceService,
+    LocationService,
     DialogConfigComponent,
     AlertComponent
   ]
@@ -27,27 +37,38 @@ import { AlertComponent } from '../../alerts/alert.component';
 export class EditPatientComponent implements OnInit {
   action: string;
   title: string;
-  message: string;
-  messageClass: string;
   patient: any;
   socialInsurances: any;
-  socialInsurance: PatientSocialInsurance;
+  socialInsurance: SocialInsurance;
   id_patient : string;
+  provinces: any;
+  cities: any;
+
+  myControlProv = new FormControl();
+  myControlCity = new FormControl();
+  myControlSIns = new FormControl();
+  filteredOptionsProv: Observable<any[]>;
+  filteredOptionsSIns: Observable<any[]>;
+  filteredOptionsCity: Observable<any[]>;
 
   constructor(private _patientService: PatientService, private _socialInsuranceService: SocialInsuranceService,
               private _router: Router, private _activatedRoute: ActivatedRoute, public dialogConfig: DialogConfigComponent,
-              public alert: AlertComponent) {
+              public alert: AlertComponent,
+              private _locationService: LocationService) {
     this.action = 'Editar';
     this.title = 'Editar Paciente';
     this.patient = new Patient();
     this.patient.socialInsurance = new PatientSocialInsurance();
+    this.socialInsurance = new SocialInsurance();
     this.patient.phones = new Phone();
     this.patient.address = new Address();
     this.socialInsurances = new Array();
-    this.socialInsurance = new PatientSocialInsurance();
+    /*Provincias*/
+    this.provinces = new Array();
   }
 
   ngOnInit() {
+    this.getProvinces();
     this.getSocialInsurances();
     this.getPatients();
   }
@@ -56,7 +77,32 @@ export class EditPatientComponent implements OnInit {
     this.savePatient();
   }
 
+  onChange() {
+    if (!this.patient.address.state) {
+      return;
+    }
+    this.patient.address.city = null;
+    this._locationService.getCities(this.patient.address.state).subscribe(
+      data => {
+        if (data[0] === undefined) {
+          this.cities = [];
+          this.filteredOptionsCity = null;
+          return;
+        }
+        this.cities = data[0].localidad;
+        this.filteredOptionsCity = this.myControlCity.valueChanges
+          .startWith(null)
+          .map(city => (city ? this.filterCity(city) : this.cities.slice()));
+        return;
+      },
+      err => {
+        this.alert.openErrorSnackBar(this.alert.genericError);
+      }
+    );
+  }
+
   savePatient() {
+    this.patient.socialInsurance._id = this.socialInsurance._id;
     this._patientService.updatePatient(this.patient, this.patient._id).subscribe(data => {
       this.alert.openSuccessSnackBar(this.alert.successPatient);
     },
@@ -70,6 +116,7 @@ export class EditPatientComponent implements OnInit {
       this.id_patient = params['id'];
       this._patientService.searchPatient(this.id_patient).subscribe(response => {
         this.patient = response;
+        this.socialInsurance = this.patient.socialInsurance._id;
       },
       err => {
         this.alert.openErrorSnackBar(this.alert.genericError);
@@ -78,12 +125,78 @@ export class EditPatientComponent implements OnInit {
   }
 
   getSocialInsurances() {
-    this._socialInsuranceService.getSocialInsurance().subscribe(response => {
-      this.socialInsurances = response;
-    },
-    err => {
-      this.alert.openErrorSnackBar(this.alert.genericError);
+    this._socialInsuranceService.getSocialInsurance().subscribe(
+      response => {
+        this.socialInsurances = response;
+        this.filteredOptionsSIns = this.myControlSIns.valueChanges
+          .startWith(null)
+          .map(
+            sIns => (sIns ? this.filterSIns(sIns) : this.socialInsurances.slice())
+          );
+      },
+      err => {
+        this.alert.openErrorSnackBar(this.alert.genericError);
+      }
+    );
+  }
+
+  getProvinces() {
+    this._locationService.getProvinces().subscribe(
+      data => {
+        this.provinces = data;
+        this.filteredOptionsProv = this.myControlProv.valueChanges
+          .startWith(null)
+          .map(
+            prov => (prov ? this.filterProvinces(prov) : this.provinces.slice())
+          );
+      },
+      err => {
+        this.alert.openErrorSnackBar(this.alert.genericError);
+      }
+    );
+  }
+
+  filterProvinces(prov: string): any[] {
+    return this.provinces.filter(option => {
+      return option.provincia.toLowerCase().indexOf(prov.toLowerCase()) === 0;
     });
+  }
+
+  filterCity(city: string): any[] {
+    return this.cities.filter(option => {
+      return option.toLowerCase().indexOf(city.toLowerCase()) === 0;
+    });
+  }
+
+  filterSIns(socIns: any): any[] {
+    if(typeof(socIns) == 'object'){
+      return this.socialInsurances.filter(option => {
+        return option.name.toLowerCase().indexOf(socIns.name.toLowerCase()) === 0;
+      });
+    };
+    if(typeof(socIns) == 'string'){
+      return this.socialInsurances.filter(option => {
+        return option.name.toLowerCase().indexOf(socIns.toLowerCase()) === 0;
+      });
+    }
+  }
+
+  displayFnProv(prov: string): string {
+    if (prov) {
+      return prov;
+    }
+  }
+
+  displayFnCity(city: string): string {
+    if (city) {
+      return city;
+    }
+  }
+
+  displaySIns(socIns: any): string {
+    if (socIns) {
+      return socIns.name;
+    }
   }
 
 }
